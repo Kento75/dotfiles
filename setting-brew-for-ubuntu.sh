95881864da625d71d90f8eded091d0353240c6df
Kento Takano <kento2github@gmail.com>
2019-10-31 17:18:04 +0900

    Update setting-brew-for-ubuntu.sh

--- setting-brew-for-ubuntu.sh
+++ setting-brew-for-ubuntu.sh
@@ -17,6 +17,23 @@ sudo apt-get install -y linuxbrew-wrapper
 ### Install Linuxbrew
 echo "Install Linuxbrew"
 echo ""
+
+sudo mkdir /home/linuxbrew/.linuxbrew/bin
+sudo mkdir /home/linuxbrew/.linuxbrew/etc
+sudo mkdir /home/linuxbrew/.linuxbrew/include
+sudo mkdir /home/linuxbrew/.linuxbrew/lib
+sudo mkdir /home/linuxbrew/.linuxbrew/sbin
+sudo mkdir /home/linuxbrew/.linuxbrew/share
+sudo mkdir /home/linuxbrew/.linuxbrew/var
+sudo mkdir /home/linuxbrew/.linuxbrew/opt
+sudo mkdir /home/linuxbrew/.linuxbrew/share/zsh
+sudo mkdir /home/linuxbrew/.linuxbrew/share/zsh/site-functions
+sudo mkdir /home/linuxbrew/.linuxbrew/var/homebrew
+sudo mkdir /home/linuxbrew/.linuxbrew/var/homebrew/linked
+sudo mkdir /home/linuxbrew/.linuxbrew/Cellar
+sudo mkdir /home/linuxbrew/.linuxbrew/Caskroom
+sudo mkdir /home/linuxbrew/.linuxbrew/Homebrew
+sudo mkdir /home/linuxbrew/.linuxbrew/Frameworks
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 echo "export PATH='/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin':$PATH" >> ~/.bashrc
 echo 'PATH="$HOME/.linuxbrew/bin:$PATH"' >> ~/.bashrc
