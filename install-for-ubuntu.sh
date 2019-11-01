128306fcfd8284e19f67d2f93eeacb5434ee6f1b
Kento75 <kento2github@gmail.com>
2019-11-01 19:20:55 +0900

    Added docker-ce install command

--- install-for-ubuntu.sh
+++ install-for-ubuntu.sh
@@ -119,7 +119,16 @@ brew install tree
 # Install Docker
 echo "Install docker"
 echo ""
-brew install docker
+#brew install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker $USER
 
 # Install docker-compose
 
