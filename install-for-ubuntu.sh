a8f2f85f4d797ebe3a7521c954f7834311bd8f58
Kento75 <kento2github@gmail.com>
2019-11-02 19:07:20 +0900

    Fixed typora install command

--- install-for-ubuntu.sh
+++ install-for-ubuntu.sh
@@ -173,7 +173,7 @@ rm -rf Sqlectron_1.30.0_amd64.deb
 wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
 sudo add-apt-repository 'deb https://typora.io/linux ./'
 sudo apt-get update
-sudo apt-get install typora
+sudo apt-get install -y typora
 
 echo "Fin"
 
