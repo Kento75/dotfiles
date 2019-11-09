490483935fd0738239cfb873f7871489b3e6edd3
Kento75 <kento2github@gmail.com>
2019-11-09 16:15:35 +0900

    Added git settings for ubuntu

--- install-for-ubuntu.sh
+++ install-for-ubuntu.sh
@@ -199,5 +199,21 @@ rm -rf terraform_0.11.13_linux_amd64.zip
 sudo cp terraform /usr/local/bin
 rm -rf terraform
 
+
+# Git Settings
+
+cat <<'EOF' >> ~/.bashrc
+if [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+fi
+if [ -f /etc/bash_completion.d/git-prompt ]; then
+    export PS1='\[\033[01;32m\]\u@\h\[\033[01;33m\] \w$(__git_ps1) \n\[\033[01;34m\]\$\[\033[00m\] '
+else
+    export PS1='\[\033[01;32m\]\u@\h\[\033[01;33m\] \w \n\[\033[01;34m\]\$\[\033[00m\] '
+fi
+EOF
+
+source ~/.bashrc
+
 echo "Fin"
 
